76ed4bc59cad845762ad2a28ca1c44cb1baf9e50
buzzrstudio <keepsoul@gmail.com>
2021-06-22 00:37:19 +0900

    User Join

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,26 @@
-export const join = (req, res) => {
-  console.log(req.body);
-  return res.end();
+import User from "../models/User";
+
+export const join = async (req, res) => {
+  const { name, email, password, password2 } = req.body;
+  const exists = await User.exists({ email });
+
+  if (exists) {
+    return res.status(400).json({ errorMessage: "이메일 주소가 이미 존재합니다." });
+  }
+
+  if (password !== password2) {
+    return res.status(400).json({ errorMessage: "비밀번호가 일치하지 않습니다." });
+  }
+
+  try {
+    await User.create({
+      name,
+      email,
+      password,
+    });
+
+    return res.status(200).json({ success: "유저 생성 완료!" });
+  } catch (error) {
+    return res.status(400).json({ errorMessage: error._message });
+  }
 };
